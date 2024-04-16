513de09383366bbd97dbd07f10476a9838cd524b
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-04-16 08:30:28 +0100

    HEAT-230 Publishing data for all environments

--- src/data/appInsights/index.ts
+++ src/data/appInsights/index.ts
@@ -1,10 +1,10 @@
-import { type Environment } from '../../config'
+import { type AppInsightsCreds } from '../../config'
 import { type Dependency } from '../Components'
 import AppInsights from './Client'
 import Queries from './queries'
 
-const getDependencies = async (env: Environment): Promise<Dependency[]> => {
-  const appInsights = new AppInsights(env.appInsightsCreds)
+const getDependencies = async (appInsightsCreds: AppInsightsCreds): Promise<Dependency[]> => {
+  const appInsights = new AppInsights(appInsightsCreds)
   const results = await appInsights.query(Queries.DEPENDENCIES())
 
   return results.rows.map(row => ({ componentName: row[0], dependencyHostname: row[1], type: row[2] }))
