f0257d09449cd88c32305c7b0aab0eac23d9c2a0
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-05-28 15:56:44 +0100

    HEAT-230 sanitise hostname

--- src/data/appInsights/index.ts
+++ src/data/appInsights/index.ts
@@ -3,11 +3,13 @@ import { type Dependency } from '../Components'
 import AppInsights from './Client'
 import Queries from './queries'
 
+export const sanitize = (s: string) => (s ? s.replace(':443', '').replace(/(.*?\.gov\.uk).*/, '$1') : '')
+
 const getDependencies = async (appInsightsCreds: AppInsightsCreds): Promise<Dependency[]> => {
   const appInsights = new AppInsights(appInsightsCreds)
   const results = await appInsights.query(Queries.DEPENDENCIES())
 
-  return results.rows.map(row => ({ componentName: row[0], dependencyHostname: row[1], type: row[2] }))
+  return results.rows.map(row => ({ componentName: row[0], dependencyHostname: sanitize(row[1]), type: row[2] }))
 }
 
 export default getDependencies
