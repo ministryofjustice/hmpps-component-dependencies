4e9c49df08abc4ad1ca4d6c463187dedb5bc6184
Marcus Aspin <maspin@unilink.com>
2024-07-10 15:41:16 +0000

    PI-2342 Capture dependencies across message boundaries

--- src/data/appInsights/queries.ts
+++ src/data/appInsights/queries.ts
@@ -1,10 +1,21 @@
 const Queries = {
   DEPENDENCIES: () => `
+      let since = ago(3d);
       dependencies
-        | where timestamp > ago(3d)
-        | project cloud_RoleName, type, target
-        | where cloud_RoleName != "" and type <> "InProc" and type <> 'Ajax'
-        | distinct cloud_RoleName, target, type
+      | where timestamp > since
+      | where isnotempty(cloud_RoleName)
+      | distinct cloud_RoleName, target, type, id=iff(name startswith "PUBLISH ", id, "")
+      | join kind=leftouter (
+          requests 
+          | where timestamp > since
+          | where isnotempty(cloud_RoleName) and name startswith "RECEIVE "
+          | distinct id = operation_ParentId, consumer_RoleName = cloud_RoleName, type = "messaging"
+          ) on id
+      | distinct
+          cloud_RoleName=iff(type1 == "messaging", consumer_RoleName, cloud_RoleName),
+          target=iff(type1 == "messaging", cloud_RoleName, target),
+          type = coalesce(type1, type)
+      | where type <> "InProc" and type <> "Ajax"
 `,
 }
 
