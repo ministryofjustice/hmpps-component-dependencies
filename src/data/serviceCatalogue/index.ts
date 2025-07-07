8682af3abc469a5f22aef24927630fce8dec1dab
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-07-07 19:58:49 +0100

    Cope with nesting in the env attributes (#34)

--- src/data/serviceCatalogue/index.ts
+++ src/data/serviceCatalogue/index.ts
@@ -5,11 +5,12 @@ const getComponents = async () => {
   const client = new Client()
   const components = await client.getComponents()
   const filteredComponents = components
-    .filter(component => component.envs?.length)
+    .filter(component => component.envs?.data?.length)
     .map(entry => ({
       name: entry.name,
       cloudRoleName: entry.app_insights_cloud_role_name,
-      envs: entry.envs
+      envs: entry.envs.data
+        .map(env => env.attributes)
         .filter(env => env.url)
         .map(env => ({
           name: env.name,
@@ -17,6 +18,7 @@ const getComponents = async () => {
           clusterHostname: `${entry.name}.${env.namespace}.svc.cluster.local`,
         })),
     }))
+
   return new Components(filteredComponents)
 }
 
