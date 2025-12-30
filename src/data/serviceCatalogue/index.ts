2fc5f7cb95c513d3e1a53f269a1a70fd29084cef
Sandhya Gandalwar <39799075+s99mj@users.noreply.github.com>
2025-12-30 14:02:56 +0000

    Updated to include empty envs (#63)
    * Updated to include empty envs
    
    * Added filter to get components where app_insights_role_name is set
    
    * Removed extra logging used for debugging

--- src/data/serviceCatalogue/index.ts
+++ src/data/serviceCatalogue/index.ts
@@ -5,17 +5,19 @@ const getComponents = async () => {
   const client = new Client()
   const components = await client.getComponents()
   const filteredComponents = components
-    .filter(component => component.envs?.length)
+    .filter(entry => entry.app_insights_cloud_role_name)
     .map(entry => ({
       name: entry.name,
       cloudRoleName: entry.app_insights_cloud_role_name,
-      envs: entry.envs
-        .filter(env => env.url)
-        .map(env => ({
-          name: env.name,
-          hostname: env.url.replace('https://', ''),
-          clusterHostname: `${entry.name}.${env.namespace}.svc.cluster.local`,
-        })),
+      envs: entry.envs?.length
+        ? entry.envs
+            .filter(env => env.url)
+            .map(env => ({
+              name: env.name,
+              hostname: env.url.replace('https://', ''),
+              clusterHostname: `${entry.name}.${env.namespace}.svc.cluster.local`,
+            }))
+        : [],
     }))
 
   return new Components(filteredComponents)
